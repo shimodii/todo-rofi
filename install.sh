341c6ea884e5e87d41bb5fdb4b08744005d1b0b1
am-shm <am.shahmoradi93@gmail.com>
2022-03-28 21:17:26 +0430

    installtion script completed!

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash 
 
-printf "
+something () {
+	clear
+	printf "
    _            _                    __ _ 
   | |_ ___   __| | ___    _ __ ___  / _(_)
   | __/ _ \ / _  |/ _ \  | '__/ _ \| |_| |
@@ -9,3 +11,22 @@ printf "
  
     .: by https://github.com/am-shm :.   
  \n"
+
+	echo
+	echo " You can change style of rofi by editing main script"
+	echo " It have 2 options: windowed and fullscreen"
+	echo 
+	echo
+}
+something
+mkdir ~/.config/rofi/
+clear
+something
+printf "[ ] copying files to your paths:"
+cp ./full_screen.rasi ~/.config/rofi/ 
+cp ./windowed.rasi ~/.config/rofi/ 
+sudo cp ./rofi_todo.sh /usr/local/bin/rofi_todo
+sleep 2
+clear
+something
+printf "[*] copying files to your paths: DONE! \n\n"
