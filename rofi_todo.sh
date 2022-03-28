911d9675f92a46460a687b008899759b6acd717d
am-shm <am.shahmoradi93@gmail.com>
2022-03-28 12:30:12 +0430

    main script developed

---
+++ rofi_todo.sh
@@ -0,0 +1,29 @@
+#!/bin/bash 
+
+first_use () {
+	mkdir ~/.todo
+	touch ~/.todo/todo_list
+	echo "+ ADD TASK" >> ~/.todo/todo_list
+}
+add_task () {
+	rofi -dmenu -p "ADD TASK:"
+}
+
+remove_task () {
+	echo
+}
+
+show_rofi() {
+	cat ~/.todo/todo_list | rofi -dmenu
+}
+
+if [[ -d ~/.todo ]] ; then
+	choice=$(show_rofi)
+	if [[ $choice = "+ ADD TASK" ]] ; then
+		NEW_TASK=$(add_task)
+		echo $NEW_TASK >> ~/.todo/todo_list
+	fi
+else
+	first_use
+	choice=$(show_rofi)
+fi
