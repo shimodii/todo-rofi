e775cf9aec78b7dd2a4375b641712ee6228a83f2
am-shm <am.shahmoradi93@gmail.com>
2022-03-28 21:32:33 +0430

    main script developed and bugs fixed!

--- rofi_todo.sh
+++ rofi_todo.sh
@@ -1,32 +1,46 @@
 #!/bin/bash 
 
+##############################################
+##           select theme here!             ##
+##############################################
+THEME="windowed.rasi"
+## AVAILABLE OPTIONS : windowed.rasi - full_screen.rasi
+##############################################
+
+
 DIR_TODO=~/.todo
 first_use () {
 	mkdir $DIR_TODO
 	cd $DIR_TODO
 	touch "+ ADD TASK"
+	touch "> EXIT"
 }
 new_task () {
-	NEW_TASK=$(rofi -dmenu -p "ADD TASK:")
+	NEW_TASK=$(rofi -dmenu -p "ADD TASK:" -theme ~/.config/rofi/task_prompt.rasi)
 	cd $DIR_TODO
 	touch "$NEW_TASK"
 }
 
 show_rofi () {
-	ls -1 $DIR_TODO | rofi -dmenu -p ">"
+	ls -1 $DIR_TODO | rofi -dmenu -p ">" -theme ~/.config/rofi/$THEME
 }
-
-if [[ -d $DIR_TODO ]] ; then
-	choice=$(show_rofi)
-	if [[ $choice = "+ ADD TASK" ]] ; then
-		NEW_TASK=$(new_task)
+while true
+do 
+	if [[ -d $DIR_TODO ]] ; then
+		choice=$(show_rofi)
+		if [[ $choice = "+ ADD TASK" ]] ; then
+			NEW_TASK=$(new_task)
+		elif [[ $choice = "> EXIT" ]] ; then
+			exit 
+		else
+			cd $DIR_TODO
+			rm "$choice"
+		fi
 	else
+		first_use
+		choice=$(show_rofi)
 		cd $DIR_TODO
 		rm "$choice"
 	fi
-else
-	first_use
-	choice=$(show_rofi)
-	cd $DIR_TODO
-	rm "$choice"
-fi
+
+done
